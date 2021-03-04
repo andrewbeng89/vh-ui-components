1243a6170629d66233cedbb2f75da66699822887
andrewbeng89 <andrewbeng89@gmail.com>
2021-03-04 13:13:36 +0800

    inject style in document head

--- rollup.config.js
+++ rollup.config.js
@@ -35,7 +35,6 @@ export default [
         plugins: [tailwind(), postcssImport()],
         module: false,
         minimize: true,
-        inject: false,
       }),
       replace({
         "process.env.NODE_ENV": JSON.stringify("production"),
